f10c1ce7d84cac7b51d0b64c905faf0c850606ee
alienorar <rayhonakhayrullaeva@gmail.com>
2025-01-07 13:15:24 +0500

    header added

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,15 +12,26 @@ const routes = [
 
 const Header = () => {
     return (
-        <div className='bg-green-800 py-4 flex justify-center' >
-            <ul className='flex items-center gap-[20px] text-white'>
-                {
-                    routes.map((item) => {
-                        return <Link key={item.id} href={item.link}>{item.title}</Link>
-                    })
-                }
-            </ul>
+        <div className="header">
+            <nav className='bg-[#233a95] py-4 flex justify-center '>
+                <p className='text-white font-sans'>Due to the <span className='font-bold'>COVID 19</span> epidemic, orders may be processed with a slight delay</p>
+            </nav>
+            <nav className='bg-[#fcfcfc] py-4 flex justify-center border-b-[1px] border-b-[#858282]' >
+                <ul className='flex items-center gap-[20px] text-black'>
+                    {
+                        routes.map((item) => {
+                            return <Link key={item.id} href={item.link}>{item.title}</Link>
+                        })
+                    }
+                </ul>
+                <div>
+                    <div>
+                        <p>100% Secure delivery without contacting the courier</p>
+                    </div>
+                </div>
+            </nav>
         </div>
+
     )
 }
 
